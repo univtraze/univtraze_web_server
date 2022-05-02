fc53120e8169e688bfe1e3ba59b399473f412f23
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-02 10:32:31 +0800

    make it better

--- auth/token_validator.js
+++ auth/token_validator.js
@@ -4,15 +4,28 @@ module.exports = {
     checkToken: (req, res, next) => {
         const token = req.get("authorization");
 
-        console.log(token)
-        // if(token){
-        //     const decode = verify(token, process.eventNames.JSON_KEY);
-        //     console.log(token)
-        // } else {
-        //     res.json({
-        //         success: 0,
-        //         message: "Access denied, unauthorized user."
-        //     })
-        // }
+        if(token){
+            verify(token, process.eventNames.JSON_KEY, (err, decoded)=>{
+                
+                console.log(token)
+
+                // if(err){
+                //     res.json({
+                //         success: 0,
+                //         message: "Invalid token"
+                //     })
+                // } else {
+                //     next();
+                // }
+            })
+
+            
+
+        } else {
+            res.json({
+                success: 0,
+                message: "Access denied, unauthorized user."
+            })
+        }
     }
 }
