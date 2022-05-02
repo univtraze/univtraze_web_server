e8307c84302acf27b6deef8fcf34864d6090f288
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-02 10:17:54 +0800

    make it better

--- auth/token_validator.js
+++ auth/token_validator.js
@@ -3,6 +3,7 @@ const { verify } = require("jsonwebtoken");
 module.exports = {
     checkToken: (req, res, next) => {
         const token = req.get("authorization");
+
         if(token){
             token = token.slice(7);
             verify(token, process.eventNames.JSON_KEY, (err, decoded)=>{
