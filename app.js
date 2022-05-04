b794f67474142ed41f87b30d6b0322cae4557998
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-04 13:18:22 +0800

    make it better

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const adminRouter = require("./api/admins/admin.router");
 
 app.use(express.json());
 app.use("/api/user", userRouter);
-app.use("/api/admin", adminRouter);
 
 const port = process.env.PORT || 3001;
 
