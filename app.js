2bb2fab79982f8dc2678dc8da78362457da66f26
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-04 13:21:17 +0800

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./api/users/user.router");
-const adminRouter = require("./api/admins/admin.router");
 
 app.use(express.json());
 app.use("/api/user", userRouter);
