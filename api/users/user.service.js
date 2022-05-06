ac0670097413e1f93cac39ef63ba0a7f35ab7a33
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-06 13:20:13 +0800

    make it better

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -325,9 +325,9 @@ module.exports = {
     },
     updateVisitorDocs: (data, callBack) => {
         pool.query(
-           `UPDATE visitor_details SET valid_id_img=?,mobile_number=?,profile_url=? WHERE user_id=?`,
+           `UPDATE visitor_details SET valid_id_img=?,mobile_number=?,email=?,profile_url=? WHERE user_id = 1`,
             [
-                data.valid_id_image,
+                data.valid_id_img,
                 data.mobile_number,
                 data.profile_url,
                 data.user_id,
