7df2ece47d3f8f2784b73d4a57dde00e2cee3af0
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-11 16:02:37 +0800

    make it better

--- api/rooms/room.controller.js
+++ api/rooms/room.controller.js
@@ -71,70 +71,75 @@ module.exports = {
                 message: "Database connection Error"
             });
             }
+
             if(results.length === 0){
-                addVisitedRoom(body, (err, results) => {
-                if(err){
-                    console.log(err)
-                 return res.json({
-                    success: 0,
-                    message: "Database connection Error"
-                 });
-                }
+                // addVisitedRoom(body, (err, results) => {
+                //     if(err){
+                //         console.log(err)
+                //         return res.json({
+                //             success: 0,
+                //             message: "Database connection Error"
+                //         });
+                //     }
 
-                return res.status(200).json({
-                    success: 1,
-                    data: results
-                });
-            });
-            }  
-
-            const date = new Date(results[0].updatedAt);
-            const dateToday = new Date();
-            
-            const year1 = date.getFullYear()
-            const month1 =date.getMonth() + 1
-            const day1 = date.getDay()
-
-            const yearNow1 = dateToday.getFullYear()
-            const monthNow1 = dateToday.getMonth() + 1
-            const dayNow1 = dateToday.getDay()
-
-            const updatedAt = year1+'-'+month1+'-'+day1
-            const currentDate = yearNow1+'-'+monthNow1+'-'+dayNow1
-
-            if(updatedAt === currentDate){
-                updateVisitedRoom(body, (err, results) => {
-                    if(err){
-                        console.log(err)
-                        return res.json({
-                            success: 0,
-                            message: "Database connection Error"
-                        });
-                    }
-               
                     return res.status(200).json({
                         success: 1,
-                        message: "Visited room date updated successfully",
-                        data: results
+                        data: "Room not yet visited"
                     });
-               
-             });
-            }
-            
-            addVisitedRoom(body, (err, results) => {
-                if(err){
-                    console.log(err)
-                 return res.json({
-                    success: 0,
-                    message: "Database connection Error"
-                 });
-                }
+                // });
+            } 
 
-                return res.status(200).json({
-                    success: 1,
-                    data: results
-                });
+            return res.status(200).json({
+                success: 1,
+                data: "Room Already Visited"
             });
+
+            // const date = new Date(results[0].updatedAt);
+            // const dateToday = new Date();
+            
+            // const year1 = date.getFullYear()
+            // const month1 =date.getMonth() + 1
+            // const day1 = date.getDay()
+
+            // const yearNow1 = dateToday.getFullYear()
+            // const monthNow1 = dateToday.getMonth() + 1
+            // const dayNow1 = dateToday.getDay()
+
+            // const updatedAt = year1+'-'+month1+'-'+day1
+            // const currentDate = yearNow1+'-'+monthNow1+'-'+dayNow1
+
+            // if(updatedAt === currentDate){
+            //     updateVisitedRoom(body, (err, results) => {
+            //         if(err){
+            //             console.log(err)
+            //             return res.json({
+            //                 success: 0,
+            //                 message: "Database connection Error"
+            //             });
+            //         }
+            //         return res.status(200).json({
+            //             success: 1,
+            //             message: "Visited room date updated successfully",
+            //             data: results
+            //         });
+               
+            //  });
+            // }
+            
+            // addVisitedRoom(body, (err, results) => {
+            //     if(err){
+            //         console.log(err)
+            //      return res.json({
+            //         success: 0,
+            //         message: "Database connection Error"
+            //      });
+            //     }
+
+            //     return res.status(200).json({
+            //         success: 1,
+            //         data: results
+            //     });
+            // });
 
             
         });
