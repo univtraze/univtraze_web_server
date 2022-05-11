a465031f5f01e04594ddf01e0638e2eca61466e0
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-11 10:13:45 +0800

    make it better

--- api/vaccination_info/vaccination.controller.js
+++ api/vaccination_info/vaccination.controller.js
@@ -1,26 +1,26 @@
 const { insertVaccineData, updateVaccineInfo } = require("./vaccination.service");
 
 module.exports = {
-    updateVaccineInfo: (req, res) => {
-        const body = req.body;
-            updateVaccineInfo(body, (err, results) => {
-                if(err){
-                    console.log(err)
-                    return res.json({
-                        success: 0,
-                        message: "Database connection Error"
-                    });
-                }
+    // updateVaccineInfo: (req, res) => {
+    //     const body = req.body;
+    //         updateVaccineInfo(body, (err, results) => {
+    //             if(err){
+    //                 console.log(err)
+    //                 return res.json({
+    //                     success: 0,
+    //                     message: "Database connection Error"
+    //                 });
+    //             }
                 
-                return res.status(200).json({
-                    success: 1,
-                    data: results
-                });
-            });
+    //             return res.status(200).json({
+    //                 success: 1,
+    //                 data: results
+    //             });
+    //         });
 
-    },
+    // },
 
-    insertVaccinationData: (req, res) => {
+    insertVaccineData: (req, res) => {
         const body = req.body;
 
         res.json(body)
