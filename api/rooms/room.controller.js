85d0171ea097344e0566fcfc1a1575ff7dee5aa6
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-11 15:57:01 +0800

    make it better

--- api/rooms/room.controller.js
+++ api/rooms/room.controller.js
@@ -104,14 +104,14 @@ module.exports = {
 
             if(updatedAt === currentDate){
                 updateVisitedRoom(body, (err, results) => {
-                    // if(err){
-                    //     console.log(err)
-                    // return res.json({
-                    //     success: 0,
-                    //     message: "Database connection Error"
-                    // });
-                    // }
-
+                    if(err){
+                        console.log(err)
+                        return res.json({
+                            success: 0,
+                            message: "Database connection Error"
+                        });
+                    }
+               
                     return res.status(200).json({
                         success: 1,
                         message: "Visited room date updated successfully",
