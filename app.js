20291ec00cc0016419b8fdff0fef9982b71ee68d
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-13 10:29:06 +0800

    make it better

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./api/users/user.router");
-const adminRouter = require("./api/admins/admin.router")
-const vaccination_info = require("./api/vaccination_info/vaccination.router")
-const roomRouter = require("./api/rooms/room.router")
+const adminRouter = require("./api/admins/admin.router");
+const vaccination_info = require("./api/vaccination_info/vaccination.router");
+const roomRouter = require("./api/rooms/room.router");
+const covidCasesRouter = require("./api/covid_cases/covid_case.router");
 
 const cors = require("cors");
 
@@ -17,6 +18,7 @@ app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/vaccine_info", vaccination_info);
 app.use("/api/rooms", roomRouter);
+app.use("/api/covid_cases", covidCasesRouter);
 
 
 const port = process.env.PORT || 3001;
