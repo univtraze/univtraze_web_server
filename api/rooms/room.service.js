216616a5019b7aeb4c3a78e92a35b615379a2027
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-13 15:36:50 +0800

    make it better

--- api/rooms/room.service.js
+++ api/rooms/room.service.js
@@ -4,7 +4,7 @@ module.exports = {
 
     checkIfRoomExists: (data, callBack) => {
         pool.query(
-            `SELECT id, room_number, building_name, room_name, createdAt, updatedAt FROM room_details WHERE room_number = ? AND building_name = ? AND room_name = ?`,
+            `SELECT id, room_number, building_name, room_name, createdAt, updatedAt FROM rooms WHERE room_number = ? AND building_name = ? AND room_name = ?`,
             [
                 data.room_number,
                 data.building_name, 
@@ -21,7 +21,7 @@ module.exports = {
    
     addRoom: (data, callBack) => {
         pool.query(
-            `INSERT INTO room_details(room_number, building_name, room_name) VALUES (?,?,?)`,
+            `INSERT INTO rooms(room_number, building_name, room_name) VALUES (?,?,?)`,
             [
                 data.room_number,
                 data.building_name, 
@@ -38,7 +38,7 @@ module.exports = {
 
     getAllRooms: (data, callBack) => {
         pool.query(
-            `SELECT id, room_number, building_name, room_name, createdAt, updatedAt FROM room_details WHERE 1`,
+            `SELECT id, room_number, building_name, room_name, createdAt, updatedAt FROM rooms WHERE 1`,
             [
             ],
             (error, results, fields) => {
