105a3392eb6dc1cc310ca58aacc772eb5be55a50
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-13 09:48:25 +0800

    make it better

--- api/rooms/room.controller.js
+++ api/rooms/room.controller.js
@@ -84,7 +84,7 @@ module.exports = {
 
                     return res.status(200).json({
                         success: 1,
-                        data: "Room not yet visited"
+                        data: "Room not yet visited uwu"
                     });
                 // });
             }
