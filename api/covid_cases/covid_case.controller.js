87693ea4a1feb71ce73ef1a355d1a52d178f84c2
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-13 11:19:39 +0800

    make it better

--- api/covid_cases/covid_case.controller.js
+++ api/covid_cases/covid_case.controller.js
@@ -3,28 +3,20 @@
 module.exports = {
     addCovidPositive: (req, res) => {
         const body = req.body;
+        addCovidPositive(body, (err, results) => {
+            if(err){
+                console.log(err)
+                return res.json({
+                    success: 0,
+                    message: "Database connection Error"
+                });
+                
+            }
 
-        return res.json({
-            success: 1,
-            message: body
+            return res.json({
+                success: 1,
+                data: results
+            });
         });
-
-        // addCovidPositive(body, (err, results) => {
-        //     if(err){
-        //         console.log(err)
-        //         return res.json({
-        //             success: 0,
-        //             message: "Database connection Error"
-        //         });
-        //     }
-
-        //     if(results.length !== 0){
-        //         return res.json({
-        //             success: 0,
-        //             message: "Email already have an account"
-        //         });
-        //     }
-
-        // });
     },
 }
---
+++ api/covid_cases/covid_case.service.js
@@ -0,0 +1,22 @@
+const pool = require("../../config/database");
+
+module.exports = {
+
+    addCovidPositive: (data, callBack) => {
+        pool.query(
+            `INSERT INTO covid_reporting(user_id, case_number, proof_docs) VALUES (?,?,?)`,
+            [
+                data.user_id, 
+                data.case_number, 
+                data.proof_docs
+            ],
+            (error, results, fields) => {
+                if(error) {
+                    return callBack(error)
+                }
+                    return callBack(null, results)
+            }
+        )
+    }
+   
+};
