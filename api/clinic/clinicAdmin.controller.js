8ee928f9f983f5be49814bfde918c34911438998
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-05-25 15:03:59 +0800

    make it better

--- api/clinic/clinicAdmin.controller.js
+++ api/clinic/clinicAdmin.controller.js
@@ -24,25 +24,20 @@ module.exports = {
                 });
             }
 
-            return res.status(200).json({
-                success: 1,
-                data: results
-            });
-
-            // createClinicAdmin(body, (err, results) => {
-            //     if(err){
-            //         console.log(err)
-            //         return res.json({
-            //             success: 0,
-            //             message: "Database connection Error"
-            //         });
-            //     }
+            createClinicAdmin(body, (err, results) => {
+                if(err){
+                    console.log(err)
+                    return res.json({
+                        success: 0,
+                        message: "Database connection Error"
+                    });
+                }
     
-            //     return res.status(200).json({
-            //         success: 1,
-            //         data: results
-            //     });
-            // });
+                return res.status(200).json({
+                    success: 1,
+                    data: results
+                });
+            });
 
         });
     }
