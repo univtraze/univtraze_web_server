fe357ae7e25f60d012f19a3ae537caddd2b91af7
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-01 10:55:01 +0800

    make it better

--- api/rooms/room.controller.js
+++ api/rooms/room.controller.js
@@ -62,34 +62,7 @@ module.exports = {
     addVisitedRoom: (req, res) => {
        
         body = req.body
-
-        checkIfRoomVisitedExists(body, (err, results) => {
-           
-            if(err){
-                console.log(err)
-                return res.json({
-                success: 0,
-                message: "Database connection Error"
-            });
-            }
-
-            if(results.length === 0){
-                addVisitedRoom(body, (err, results) => {
-                    if(err){
-                        console.log(err)
-                        return res.json({
-                            success: 0,
-                            message: "Database connection Error"
-                        });
-                    }
-
-                    return res.status(200).json({
-                        success: 1,
-                        data: results
-                    });
-                });
-            } 
-
+        
             addVisitedRoom(body, (err, results) => {
                 if(err){
                     console.log(err)
@@ -106,8 +79,6 @@ module.exports = {
                 });
             });
 
-            
-        });
 
 
     },
@@ -129,8 +100,14 @@ module.exports = {
             });
         })
     },
+
     userVisitedRooms: (req, res) => {
         const body = req.body;
+
+        return res.status(200).json({
+                    success: 1,
+                    data: body
+                });
 
         // userVisitedRooms(body, (err, results ) => {
 
