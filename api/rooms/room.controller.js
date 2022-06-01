94de846bd29e900dbc56350ce82eee334e656bb2
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-01 10:58:06 +0800

    make it better

--- api/rooms/room.controller.js
+++ api/rooms/room.controller.js
@@ -62,7 +62,34 @@ module.exports = {
     addVisitedRoom: (req, res) => {
        
         body = req.body
-        
+
+        checkIfRoomVisitedExists(body, (err, results) => {
+           
+            if(err){
+                console.log(err)
+                return res.json({
+                success: 0,
+                message: "Database connection Error"
+            });
+            }
+
+            if(results.length === 0){
+                addVisitedRoom(body, (err, results) => {
+                    if(err){
+                        console.log(err)
+                        return res.json({
+                            success: 0,
+                            message: "Database connection Error"
+                        });
+                    }
+
+                    return res.status(200).json({
+                        success: 1,
+                        data: results
+                    });
+                });
+            } 
+
             addVisitedRoom(body, (err, results) => {
                 if(err){
                     console.log(err)
@@ -79,6 +106,8 @@ module.exports = {
                 });
             });
 
+            console.log(err)
+        });
 
 
     },
@@ -100,31 +129,25 @@ module.exports = {
             });
         })
     },
-
     userVisitedRooms: (req, res) => {
         const body = req.body;
 
-        return res.status(200).json({
-                    success: 1,
-                    data: body
+        userVisitedRooms(body, (err, results ) => {
+
+            if(err){
+                console.log(err)
+                return res.json({
+                    success: 0,
+                    message: "Database connection Error"
                 });
-
-        // userVisitedRooms(body, (err, results ) => {
-
-        //     if(err){
-        //         console.log(err)
-        //         return res.json({
-        //             success: 0,
-        //             message: "Database connection Error"
-        //         });
-        //     }
-        //     return res.status(200).json({
-        //         success: 1,
-        //         data: results
-        //     });
+            }
+            return res.status(200).json({
+                success: 1,
+                data: results
+            });
 
 
-        // })
+        })
     }
 
 
