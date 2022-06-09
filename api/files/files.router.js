adb58b30f5b02e8dd8d4fe5a47832e6b8fa02361
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-09 15:52:19 +0800

    make it better

--- api/files/files.router.js
+++ api/files/files.router.js
@@ -1,8 +1,7 @@
-const router = require("express").Router();
-// const { checkToken } = require("../../auth/token_validator");
+const router = require("express").Router()
 const {  uploadUserImageProfile } = require("./files.controller");
 
-router.post("/uploadUserImageProfile", checkToken,  uploadUserImageProfile)
+router.post("/uploadUserImageProfile", uploadUserImageProfile)
 // router.post("/uploadUserId", checkToken, searchEmergencyReportsViaDate)
 // router.post("/uploadUserProofDoc", checkToken, searchEmergencyReportsViaDate)
 
