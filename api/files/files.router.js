f497678e66477255b6681fdd9781fd9add76cc1f
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-09 16:13:49 +0800

    make it better

--- api/files/files.router.js
+++ api/files/files.router.js
@@ -1,8 +1,9 @@
 const router = require("express").Router()
 // const {  uploadUserImageProfile } = require("./files.controller");
 
-router.post("/uploadUserImageProfile", (req, res) => {
+router.post("/uploadUserImageProfile", function (req, res) {
     const body = req.body;
+
     return res.json({
         success: 1,
         data: body
