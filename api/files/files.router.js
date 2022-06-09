99f7dcac17eb37bd31fa3257be02ff20a3f656b1
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-09 16:19:09 +0800

    make it better

--- api/files/files.router.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const { checkToken } = require("../../auth/token_validator");
-const {  uploadUserImageProfile } = require("./files.controller");
-
-router.post("/uploadUserImageProfile", checkToken, uploadUserImageProfile)
-
-module.exports = router;
