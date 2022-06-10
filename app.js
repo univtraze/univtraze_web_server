a445f97e7a5cc8bec8346daf9e3d2d301b937836
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-10 19:12:00 +0800

    make it better

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors({origin: "*"}));
 
+app.use(bodyParser.urlencoded({extended: true}));
 var bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
