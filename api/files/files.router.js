03f5f42399515c04e6fb3e2901223d785b544e23
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-13 12:57:54 +0800

    Upload image

--- api/files/files.router.js
+++ api/files/files.router.js
@@ -1,38 +1,47 @@
 const router = require("express").Router()
 const cloudinary = require("../../config/cloudinary-config")
+const base64 = require('base-64');
 
 router.post("/uploadUserImageProfile", async (req, res) => {
    
     const { image } = req.body;
     
-    try {
-        if(image){
-          const uploadRes =  await cloudinary.uploader.upload(image, {
-                upload_preset: "univtraze_app"
-            })
+    const decodedImage = base64.decode(image)
+
+    return res.json({
+        success: 0,
+        message: decodedImage,
+    });
+
+
+    // try {
+    //     if(image){
+    //       const uploadRes =  await cloudinary.uploader.upload(image, {
+    //             upload_preset: "univtraze_app"
+    //         })
           
-            if(uploadRes){
-                return res.json({
-                    success: 1,
-                    message: "Image added successfully",
-                    data: uploadRes
-                });
-            }
+    //         if(uploadRes){
+    //             return res.json({
+    //                 success: 1,
+    //                 message: "Image added successfully",
+    //                 data: uploadRes
+    //             });
+    //         }
 
-            return res.json({
-                success: 0,
-                message: "Failed uploading image to cloudinary",
-                data: uploadRes
-            });
+    //         return res.json({
+    //             success: 0,
+    //             message: "Failed uploading image to cloudinary",
+    //             data: uploadRes
+    //         });
 
-        }
+    //     }
 
-    } catch (error) {
-        return res.json({
-            success: 0,
-            message: "Failed uploading image to cloudinary : " + error,
-        });
-    }
+    // } catch (error) {
+    //     return res.json({
+    //         success: 0,
+    //         message: "Failed uploading image to cloudinary : " + error,
+    //     });
+    // }
 })
 
 module.exports = router;
