8d235666d1a3f8b79e27eb62e57e8936d6e43885
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-13 12:48:14 +0800

    Upload image

--- app.js
+++ app.js
@@ -25,15 +25,9 @@ const fileUpload = multer()
 
 app.use(express.json());
 app.use(cors({origin: "*"}));
-app.use(bodyParser.json({
-  limit: '50mb'
-}));
-
-app.use(bodyParser.urlencoded({
-  limit: '50mb',
-  parameterLimit: 100000,
-  extended: true 
-}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json());
 
 
 app.use("/api/user", userRouter);
