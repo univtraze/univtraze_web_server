056fc0bc33060c534e4371c30c434429f2964a3c
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-13 15:01:06 +0800

    Upload image

--- api/files/files.router.js
+++ api/files/files.router.js
@@ -1,70 +1,72 @@
 const router = require("express").Router()
 const cloudinary = require("../../config/cloudinary-config")
+const multer = require('multer')
+const streamifier = require('streamifier')
+const fileUpload = multer()
 
 
-router.post("/uploadUserImageProfile", async (req, res) => {
-    const imageFor = req.body.imageFor;
-    const image = req.body.image;
+
+router.post("/uploadUserImageProfile", fileUpload.single('image'),  async (req, res) => {
     
-    if(imageFor === "profile/image"){
-        try {
-            if(image){
-              const uploadRes =  await cloudinary.uploader.upload(image, {
-                    upload_preset: "univtraze_app_photos"
-                })
-              
-                if(uploadRes){
-                    return res.json({
-                        success: 1,
-                        message: "Image added successfully",
-                        data: uploadRes
-                    });
+        let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+              (error, result) => {
+                if (result) {
+                  resolve(result);
+                } else {
+                  res.json({
+                      success: 0,
+                      message: 'Uploading image failed, try again later'
+                  })
+                  reject(error);
                 }
-    
-                return res.json({
-                    success: 0,
-                    message: "Failed uploading image to cloudinary",
-                    data: uploadRes
-                });
-    
-            }
-    
-        } catch (error) {
-            return res.json({
-                success: 0,
-                message: "Failed uploading image to cloudinary : " + error,
-            });
-        }   
-    }
+              }
+            );
 
-    try {
-        if(image){
-          const uploadRes =  await cloudinary.uploader.upload(image, {
-                upload_preset: "univtraze_app"
-            })
-          
-            if(uploadRes){
-                return res.json({
-                    success: 1,
-                    message: "Image added successfully",
-                    data: uploadRes
-                });
-            }
-
-            return res.json({
-                success: 0,
-                message: "Failed uploading image to cloudinary",
-                data: uploadRes
-            });
-
-        }
-
-    } catch (error) {
-        return res.json({
-            success: 0,
-            message: "Failed uploading image to cloudinary : " + error,
+          streamifier.createReadStream(req.file.buffer).pipe(stream);
         });
-    }   
+    };
+
+    async function upload(req) {
+        let result = await streamUpload(req);
+        res.status(200).json({
+            success: 1,
+            message: 'Image added successfully',
+            data: result
+        })
+    }
+    
+    upload(req);
+
+    // try {
+    //     if(image){
+    //       const uploadRes =  await cloudinary.uploader.upload(image, {
+    //             upload_preset: "univtraze_app"
+    //         })
+          
+    //         if(uploadRes){
+    //             return res.json({
+    //                 success: 1,
+    //                 message: "Image added successfully",
+    //                 data: uploadRes
+    //             });
+    //         }
+
+    //         return res.json({
+    //             success: 0,
+    //             message: "Failed uploading image to cloudinary",
+    //             data: uploadRes
+    //         });
+
+    //     }
+
+    // } catch (error) {
+    //     return res.json({
+    //         success: 0,
+    //         message: "Failed uploading image to cloudinary : " + error,
+    //     });
+    // }   
 })
 
 module.exports = router;
