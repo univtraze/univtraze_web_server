afae5caa0d5f5332fd0655972b5ac2ee2b54e166
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-13 13:27:35 +0800

    Upload image

--- api/files/files.router.js
+++ api/files/files.router.js
@@ -37,41 +37,10 @@ router.post("/uploadUserImageProfile", async (req, res) => {
         }   
     }
 
-    if(imageFor === "id/image"){
-        try {
-            if(image){
-              const uploadRes =  await cloudinary.uploader.upload(image, {
-                    upload_preset: "univtraze_app_photos"
-                })
-              
-                if(uploadRes){
-                    return res.json({
-                        success: 1,
-                        message: "Image added successfully",
-                        data: uploadRes
-                    });
-                }
-    
-                return res.json({
-                    success: 0,
-                    message: "Failed uploading image to cloudinary",
-                    data: uploadRes
-                });
-    
-            }
-    
-        } catch (error) {
-            return res.json({
-                success: 0,
-                message: "Failed uploading image to cloudinary : " + error,
-            });
-        }   
-    }
-
     try {
         if(image){
           const uploadRes =  await cloudinary.uploader.upload(image, {
-                upload_preset: "univtraze_app_id"
+                upload_preset: "univtraze_app"
             })
           
             if(uploadRes){
