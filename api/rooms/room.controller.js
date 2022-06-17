a2c9d9eae8086e665b59274a7614ccd7e0bd0e15
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-17 16:13:31 +0800

    make it better

--- api/rooms/room.controller.js
+++ api/rooms/room.controller.js
@@ -140,7 +140,7 @@ module.exports = {
 
             return res.status(200).json({
                 success: 1,
-                data: results
+                data: results[0]
             });
             
 
