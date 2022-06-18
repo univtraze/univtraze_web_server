584302a4afcca03d492d4a863f990e2f5f806be3
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-18 18:26:28 +0800

    Uploading data from Add communicable disease case

--- api/covid_cases/covid_case.controller.js
+++ api/covid_cases/covid_case.controller.js
@@ -3,7 +3,28 @@ const { addCovidPositive, addEmergencyReport, addDailyAssessement, searchEmergen
 
 module.exports = {
     addCommunicableDiseaseCase: (req, res) => {
+        
         const body = req.body;
+
+        addCommunicableDiseaseCase(body, (err, results) => {
+            if(err){
+                console.log(err)
+                return res.json({
+                
+                    success: 0,
+                    message: "Database connection Error"
+                
+                });
+                
+            }
+
+            return res.json({
+                success: 1,
+                data: results
+            });
+        });
+        
+
 
         return res.json({
             success: 1,
