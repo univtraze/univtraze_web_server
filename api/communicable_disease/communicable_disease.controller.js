dc7c081a476dab8dfd28e42b3f67b1a1370c628f
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-24 14:55:15 +0800

    Yoyoyoyo

--- api/communicable_disease/communicable_disease.controller.js
+++ api/communicable_disease/communicable_disease.controller.js
@@ -38,7 +38,7 @@ module.exports = {
                             return results.data
                         });
 
-                  return allDisease.push({disease_name: disease.disease_name, total: allDiseaseData.length, records: allDiseaseData})
+                  return allDisease.push({disease_name: disease.disease_name, records: allDiseaseData})
 
                 })
               ).then((res) => {
