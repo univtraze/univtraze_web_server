e418d6122b463a7d33aa1e56a7909d6eac77b473
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-24 16:26:57 +0800

    Yoyoyoyo

--- api/communicable_disease/communicable_disease.controller.js
+++ api/communicable_disease/communicable_disease.controller.js
@@ -37,7 +37,7 @@ module.exports = {
                             });                                
                             }
 
-                        return resultData.push(...resultData, results)
+                        return resultData.push([...resultData, results])
                     
                     });
 
