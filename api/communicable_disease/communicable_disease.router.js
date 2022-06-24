66108c44f52d4ab24a7a89f8a05632b7fe1cedbc
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-24 13:50:54 +0800

    Yoyoyoyo

--- api/communicable_disease/communicable_disease.router.js
+++ api/communicable_disease/communicable_disease.router.js
@@ -3,6 +3,6 @@ const {checkToken } = require("../../auth/token_validator");
 const {getAllCommunicableDisease, getCommunicableDiseaseByName}  = require('./communicable_disease.controller')
 
 router.get("/getAllCommunicableDisease", checkToken, getAllCommunicableDisease)
-router.get("/getCommunicableDiseaseByName", checkToken, getCommunicableDiseaseByName)
+router.post("/getCommunicableDiseaseByName", checkToken, getCommunicableDiseaseByName)
 
 module.exports = router;
