077fc477e272cc394c9298aa64fdffe5c416702c
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-24 15:43:13 +0800

    Yoyoyoyo

--- api/communicable_disease/communicable_disease.controller.js
+++ api/communicable_disease/communicable_disease.controller.js
@@ -25,22 +25,24 @@ module.exports = {
 
             results.map(async (disease) => {
                 
-                var diseaseData = [];
-
                 await getCommunicableDiseaseByName(disease, (err, results) => {
-                        if(err){
-                                    console.log(err)
-                                    return res.json({
-                                        success: 0,
-                                        message: "Database connection Error"
-                                    });                                
-                                }
                         
-                        return console.log(disease.disease_name + " => " + results)
+                    var diseaseData = [];
+
+                        if(err){
+                            console.log(err)
+                            return res.json({
+                                     success: 0,
+                                     message: "Database connection Error"
+                                });                                
+                             }
+                        
+                        diseaseData.push({disease_name: disease.disease_name, data: results})
+                        return 
                         
                         });
 
-                return allDisease.push(diseaseData)
+                    return allDisease.push(diseaseData)
 
                 })
 
