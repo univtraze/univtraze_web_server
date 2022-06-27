26624ae72a4c18d521ff1719a328c84f5fac0749
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-27 15:35:37 +0800

    make it better

--- api/clinic/clinicAdmin.controller.js
+++ api/clinic/clinicAdmin.controller.js
@@ -8,38 +8,42 @@ module.exports = {
         const salt = genSaltSync(10);
         body.password = hashSync(body.password, salt)
 
-        emailClinicAdminCheck(body, (err, results) => {
-            if(err){
-                console.log(err)
-                return res.json({
-                    success: 0,
-                    message: "Database connection Error"
-                });
-            }
 
-            if(results.length !== 0){
-                return res.json({
-                    success: 0,
-                    message: "Email already have an account"
-                });
-            }
+        return res.json({
+            data: body
+        })
+        // emailClinicAdminCheck(body, (err, results) => {
+        //     if(err){
+        //         console.log(err)
+        //         return res.json({
+        //             success: 0,
+        //             message: "Database connection Error"
+        //         });
+        //     }
 
-            createClinicAdmin(body, (err, results) => {
-                if(err){
-                    console.log(err)
-                    return res.json({
-                        success: 0,
-                        message: "Database connection Error"
-                    });
-                }
+        //     if(results.length !== 0){
+        //         return res.json({
+        //             success: 0,
+        //             message: "Email already have an account"
+        //         });
+        //     }
+
+        //     createClinicAdmin(body, (err, results) => {
+        //         if(err){
+        //             console.log(err)
+        //             return res.json({
+        //                 success: 0,
+        //                 message: "Database connection Error"
+        //             });
+        //         }
     
-                return res.status(200).json({
-                    success: 1,
-                    data: results
-                });
-            });
+        //         return res.status(200).json({
+        //             success: 1,
+        //             data: results
+        //         });
+        //     });
 
-        });
+        // });
     },
 
     
