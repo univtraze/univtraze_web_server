cae48c02e59c3fedac4b3125496bf32c4b8c215a
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-27 15:38:22 +0800

    make it better

--- api/clinic/clinicAdmin.controller.js
+++ api/clinic/clinicAdmin.controller.js
@@ -8,80 +8,80 @@ module.exports = {
         const salt = genSaltSync(10);
         body.password = hashSync(body.password, salt)
 
+        emailClinicAdminCheck(body, (err, results) => {
+            if(err){
+                console.log(err)
+                return res.json({
+                    success: 0,
+                    message: "Database connection Error"
+                });
+            }
 
-        return res.json({
-            data: body
-        })
-        // emailClinicAdminCheck(body, (err, results) => {
-        //     if(err){
-        //         console.log(err)
-        //         return res.json({
-        //             success: 0,
-        //             message: "Database connection Error"
-        //         });
-        //     }
+            if(results.length !== 0){
+                return res.json({
+                    success: 0,
+                    message: "Email already have an account"
+                });
+            }
 
-        //     if(results.length !== 0){
-        //         return res.json({
-        //             success: 0,
-        //             message: "Email already have an account"
-        //         });
-        //     }
-
-        //     createClinicAdmin(body, (err, results) => {
-        //         if(err){
-        //             console.log(err)
-        //             return res.json({
-        //                 success: 0,
-        //                 message: "Database connection Error"
-        //             });
-        //         }
+            createClinicAdmin(body, (err, results) => {
+                if(err){
+                    console.log(err)
+                    return res.json({
+                        success: 0,
+                        message: "Database connection Error"
+                    });
+                }
     
-        //         return res.status(200).json({
-        //             success: 1,
-        //             data: results
-        //         });
-        //     });
+                return res.status(200).json({
+                    success: 1,
+                    data: results
+                });
+            });
 
-        // });
+        });
     },
 
     
     loginClinicAdmin: (req, res) => {
         const body = req.body;
 
-        getClinicAdminByEmail(body, (err, results) => {
-            if(err) {
-                console.log(err );
-                return
-            }
-            if(!results){
-                return res.json({
-                    success: 0,
-                    data: "Incorrect Email or Password"
-                })
-            }
+        return res.json({
+                        success: 0,
+                        data: body
+                    })
+        // getClinicAdminByEmail(body, (err, results) => {
+        //     if(err) {
+        //         console.log(err );
+        //         return
+        //     }
+        //     if(!results){
+        //         return res.json({
+        //             success: 0,
+        //             data: "Incorrect Email or Password"
+        //         })
+        //     }
 
-            const result = compareSync(body.password, results.password);
+        //     const result = compareSync(body.password, results.password);
 
-            if(result) {
-                result.password = undefined;
-                const jsonToken = sign({result: results}, process.env.JSON_KEY, {
-                    expiresIn: "7d"
-                })
+        //     if(result) {
+        //         result.password = undefined;
+        //         const jsonToken = sign({result: results}, process.env.JSON_KEY, {
+        //             expiresIn: "7d"
+        //         })
 
-                return res.json({
-                    success: 1,
-                    message: "Login successfully",
-                    token: jsonToken
-                });
-            } else {
-                return res.json({
-                    success: 0,
-                    data: "Incorrect Email or Password"
-                })
-            }
-        });
+        //         return res.json({
+        //             success: 1,
+        //             message: "Login successfully",
+        //             token: jsonToken
+        //         });
+        //     } else {
+        //         return res.json({
+        //             success: 0,
+        //             data: "Incorrect Email or Password"
+        //         })
+        //     }
+        // });
 
     }
 
