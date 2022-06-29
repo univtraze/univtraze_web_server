4d64e69c8d431ef4833198c7750194988502ddce
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-06-29 16:59:20 +0800

    Update case status

--- api/communicable_disease/communicable_disease.controller.js
+++ api/communicable_disease/communicable_disease.controller.js
@@ -130,7 +130,7 @@ module.exports = {
         var start_date = new Date(body.date_reported);
         start_date.setDate(start_date.getDate() - body.date_range);
 
-        body['start_date'] = start_date.toISOString().replace(/T/, ' ').replace(/\..+/, '')
+        body['start_date'] = start_date.format("YYYY-MM-DD")
         body['end_date'] = new Date(body.date_reported).toISOString().replace(/T/, ' ').replace(/\..+/, '')
 
 
