7962e4bb7200f7ef1e9a1fc37442130e03fa0232
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-07-02 13:03:11 +0800

    First degree

--- api/communicable_disease/communicable_disease.service.js
+++ api/communicable_disease/communicable_disease.service.js
@@ -16,9 +16,9 @@ module.exports = {
 
     getCommunicableDiseaseByName: (data, callBack) => {
         pool.query(
-            `SELECT id, user_id, type, disease_name, document_proof_image, createAt, updatedAt FROM communicable_disease_reporting WHERE disease_name LIKE %?%`,
+            `SELECT id, user_id, type, disease_name, document_proof_image, createAt, updatedAt FROM communicable_disease_reporting WHERE disease_name LIKE ?`,
             [
-                data.disease_name,
+                "%"+data.disease_name+"%",
             ],
             (error, results, fields) => {
                 if(error) {
