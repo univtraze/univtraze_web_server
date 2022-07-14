692a2e04ef842eabde456677a352bf83dc53a365
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-07-14 17:30:39 +0800

    First degree with names on it uwu

--- api/rooms/room.controller.js
+++ api/rooms/room.controller.js
@@ -290,6 +290,7 @@ module.exports = {
 
             return res.status(200).json({
                 success: 1,
+                config: body,
                 data: queryResults
             });
         })
