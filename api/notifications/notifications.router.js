6cfadd9c19b28b6c2149a0311eacf1ff3c3380ed
JayCabasag <79354395+JayCabasag@users.noreply.github.com>
2022-08-06 21:34:56 +0800

    Update on clinic notifications

--- api/notifications/notifications.router.js
+++ api/notifications/notifications.router.js
@@ -6,7 +6,7 @@ router.post("/getAdminNotifications", checkToken, adminNotifications)
 router.post("/updateAdminNotificationStatus", checkToken, updateAdminNotificationStatus)
 router.get("/getTotalActiveAdminNotifications", checkToken, getTotalActiveAdminNotifications)
 router.post("/getClinicNotifications", checkToken, getClinicNotifications)
-router.post("/getTotalActiveClinicNotifications", checkToken, getTotalActiveClinicNotifications)
+router.get("/getTotalActiveClinicNotifications", checkToken, getTotalActiveClinicNotifications)
 router.post("/updateClinicNotificationStatus", checkToken, updateClinicNotificationsStatus)
 
 module.exports = router;
